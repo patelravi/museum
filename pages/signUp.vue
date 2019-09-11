602bfad5a74bd4448e7a20a9234c9d6353858207
Jigar Kumar <ravipatel.ce@gmail.com>
2019-09-11 15:27:08 +0530

    Added signup page.

---
+++ pages/signUp.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="signUpPage">
+    <div class="leftSide">
+      <div class="edenTitle">
+        <h1>EDEN</h1>
+      </div>
+      <!-- <div class="title">
+        Create your accout now
+      </div> -->
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" src="@/assets/styles/signUp.scss">
+</style>
